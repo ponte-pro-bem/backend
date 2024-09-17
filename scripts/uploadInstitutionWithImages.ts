bc3d8ae231728e2ad014edc08ea53bc33114d97c
PedroGaya <pedrohenriquegaya@gmail.com>
2024-09-17 14:24:47 -0300

    upload image script

--- scripts/uploadInstitutionWithImages.ts
+++ scripts/uploadInstitutionWithImages.ts
@@ -1,18 +1,32 @@
-import { prisma } from "../libs/prisma"
+import { readdir } from "node:fs/promises";
 
+import { prisma } from "../libs/prisma"
 import { uploadImageBuffer } from "../src/crud/bucket";
+import { CreateInstitutionInput } from "../src/types";
+import { createInstitution } from "../src/crud/institution";
+import { createImage } from "../src/crud/image";
+
 
 async function main() {
-  // Inserting an institution
-  const institution = await prisma.institution.create({
-    data: {
-      name: "Charity Organization",
-      description: "A charity organization",
-      pixQRCodeRaw: "raw_qr_code_data",
-    },
-  });
+  const institutionsDirList = await readdir("./data/institutions")
+  for (const dir in institutionsDirList) {
+    const basePath = `./data/institutions/${dir}`
 
-  
+    const data: CreateInstitutionInput = await Bun.file(basePath+"/data").json()
+    const institution = await createInstitution(data)
+
+    const images = await readdir(basePath+"./images");
+    for (const img in images) {
+      const f = Bun.file(`${basePath}/images/${img}`)
+      const imgBuffer =  Buffer.from(await f.arrayBuffer())
+
+      const output = await uploadImageBuffer(img, imgBuffer)
+      await createImage({
+        key: img,
+        institutionId: institution.id
+      })
+    }
+  }
 }
 
 main()
